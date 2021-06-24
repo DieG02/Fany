48a98576e569126c368b52446cf9bd563dda0c66
DieG02 <43993554@alejandrovolta.edu.ar>
2021-06-23 23:28:00 -0300

    trying to modify request response, exceeded my diary request

--- views/Search/Search.jsx
+++ views/Search/Search.jsx
@@ -1,24 +1,19 @@
 import React, { useState, useEffect } from 'react'
 import {
   View,
+  Text,
   TextInput,
   Keyboard,
-  Text,
-  TouchableHighlight,
-  Dimensions,
+  TouchableOpacity,
   StyleSheet,
   StatusBar,
-  ScrollView,
+  FlatList
 } from 'react-native'
 import { useDispatch, useSelector } from 'react-redux'
-import { showMenu } from '../../redux/actions/uiAction.js'
+import { showMenu } from '../../redux/actions/uiAction'
 import { useIsFocused } from '@react-navigation/native'
-import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
-import {
-  faArrowLeft,
-  faTimes,
-} from '@fortawesome/free-solid-svg-icons'
-import Result from './Result.js'
+import { Entypo } from '@expo/vector-icons';
+import Result from './Result'
 import { 
   loadFontsAsync,
   WHITE,
@@ -30,36 +25,64 @@ import {
 
 const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY
 
-// ----- CONSTANTS ----- //
-const { height, width } = Dimensions.get('window')
-const _light = '#eeeeee',
-      _grey = '#cccccc',
-      _dark = '#151515';
-
-
-function FocusAwareStatusBar(props) {
+// Custom components
+const FocusAwareStatusBar = (props) => {
   const isFocused = useIsFocused();
-  return isFocused ? <StatusBar {...props} /> : null;
+  return isFocused && <StatusBar {...props} />
 }
+const renderItem = ({ item: {id, snippet} = {} }) => {
+  // Equivalent to --> const { id, snippet } = item
+  return <Result videoId={id.videoId} data={snippet} />
+};
 
 
-// ----- COMPONENT ----- //
+// Main component
 export default function Search({ navigation }) {
 
   const [value, setValue] = useState('');
   const [data, setData] = useState({});
-  const [loaded, setLoaded] = useState(false);
+  const [results, setResults] = useState([]);
+  const [request, setRequest] = useState({
+    base: 'https://youtube.googleapis.com/youtube/v3/search?part=snippet',
+    query: '',
+    key: `&key=${GOOGLE_API_KEY}`,
+    max: '&maxResults=2',
+    order: '&order=viewCount',
+    next: '',
+  })
 
+  const [loaded, setLoaded] = useState(false);
   const sound = useSelector(state => state.audio.sound);
-  let input =  value.replace(/\s+/g, '%20') 
   const dispatch = useDispatch()
 
   const search = function(){
-    fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q=${input}&key=${GOOGLE_API_KEY}`)
-    .then(res => res.json())
-    .then((data) => {
-      setData(data)
-    })
+    const { base, query, key, max, order } = request;
+    const req = `${base}&q=${query}${key}${max}${order}`;
+    console.log(req);
+    fetch(req)
+      .then(res => res.json())
+      .then((data) => {
+        setData(data);
+        console.log(data.items)
+        // setResults([data.items]);
+        setRequest({ ...request, next: data.nextPageToken })
+      })
+  }
+
+  const handleOnChange = (string) => {
+    setValue(string)
+    setRequest({ ...request, query: string.replace(/\s+/g, '%20') })
+  }
+
+  const handleOnInfinityScroll = () => {
+    const { base, query, key, max, order, next } = request;
+    fetch(`${base}&q=${query}${key}${max}${order}&pageToken=${next}`)
+      .then(res => res.json())
+      .then((data) => {
+        setData(data);
+        setResults([...results, ...data.items]);
+        setRequest({ ...request, next: data.nextPageToken })
+      })
   }
 
   useEffect(() => {
@@ -82,66 +105,46 @@ export default function Search({ navigation }) {
        />
 
       <View style={ styles.searchBar }>
-        <TouchableHighlight
+        <TouchableOpacity
           style={ styles.icon }
-          activeOpacity={ 0.8 }
-          onPress={() => {
-            navigation.goBack()
-          }}
+          activeOpacity={0.1}
+          onPress={() => navigation.goBack()}
         >
-          <FontAwesomeIcon
-            icon={ faArrowLeft }
-            size={ 23 }
-            color={ _light }
-          />
-        </TouchableHighlight>
+          <Entypo name='chevron-left' size={25} color={LIGHT} />
+        </TouchableOpacity>
         <TextInput
           autoFocus
+          value={value}
           style={ styles.input }
           placeholder='Enter name or URL'
           placeholderTextColor={GREY}
-          value={ value }
-          onChangeText={(val) => setValue(val)}
-          onSubmitEditing={() => {
-            search();
-          }}  // --> Add another search button 
+          onChangeText={handleOnChange}
+          onSubmitEditing={() => search()}  // --> Add another search button 
         />
-        <TouchableHighlight
+        <TouchableOpacity
           style={ styles.icon }
-          activeOpacity={ 0.8 }
-          onPress={() => setValue('')}
+          activeOpacity={0.1}
+          onPress={() => handleOnChange('')}
         >
-          <FontAwesomeIcon
-            icon={ faTimes }
-            size={ 23 }
-            color={ _light }
-          />
-        </TouchableHighlight>
+          <Entypo name='cross' size={25} color={LIGHT} />
+        </TouchableOpacity>
       </View>
+      {results && results.length
+      ? <FlatList
+          style={{ padding: 15 }}
+          contentContainerStyle={{ paddingBottom: 15 }}
+          data={results}
+          renderItem={renderItem}
+          keyExtractor={(_, index) => index.toString()}
+          // onEndReached={() => handleOnInfinityScroll()}
+          onEndReachedThreshold={0.3}
+        />
+      : <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', width: '100%' }}>
+          <Text style={styles.alternativeText}>No results found :(</Text>
+        </View> 
+      }
+      <TouchableOpacity onPress={() => handleOnInfinityScroll()}><Text style={{ color: "#FFF"}}>Press me for more results</Text></TouchableOpacity>
 
-     <View style={ styles.container }>
-        
-        <View style={{ height: '100%', marginTop: '5%' }}>
-          <ScrollView
-            style={ styles.scroll }
-            showsVerticalScrollIndicator={ false }
-          >
-            <View style={{ marginBottom: Object.entries(sound).length ? '23 %' : 10 }}>
-              {data.items 
-              ? data.items.map((item, index) => {
-                return (
-                  <Result 
-                    key={ index } 
-                    videoId={ item.id.videoId }
-                    data={ item.snippet }           
-                  />
-                )
-              })
-              : <Text style={ styles.alternativeText }>Your search results will appear here!</Text>}
-            </View>           
-          </ScrollView>
-        </View>
-      </View>
     </View>
   )
 }
@@ -150,56 +153,34 @@ export default function Search({ navigation }) {
 // ----- STYLERS ----- //
 const styles = StyleSheet.create({
   main: {
-    flex: 1,  
     alignItems: 'flex-start',
-    backgroundColor: _dark,
-    width: width,
-    height: height,
+    backgroundColor: DARK,
+    width: '100%',
+    height: '100%',
   },
   searchBar: {
     backgroundColor: '#303030',
     width: '100%',
     height: 50,
-    justifyContent: 'flex-start',
+    justifyContent: 'space-between',
     flexDirection: 'row',
   },
-  container: {
-    height: '88%', 
-    width: '100%',
-    paddingLeft: 15,
-    paddingRight: 15,
-  },
   input: {
-    width: '76%',
-    color: _light,
+    width: '78%',
+    color: WHITE,
     fontSize: 14,
     fontFamily: 'Poppins',
-    marginTop: 5,
+    paddingTop: 3,
+    paddingLeft: 3,
   },
   icon: {
-    width: '12%',
-    backgroundColor: 'transparent',
+    width: '11%',
     alignItems: 'center',
     justifyContent: 'center',
   },
-  subtitleContainer: {
-    justifyContent: 'flex-end',
-    paddingBottom: 10,
-    height: '10%',
-  },
-  subtitle: {
-    color: '#fff',
-    fontSize: 22,
-    fontWeight: 'bold',
-  },
-  scroll: {
-    width: '100%',
-  },
   alternativeText: {
-    color: _grey,
-    fontSize: 15,
-    fontWeight: 'bold',
-    width: width,
-    marginTop: '5%',
+    color: GREY,
+    fontSize: 17,
+    fontFamily: 'Poppins-SemiBold',
   }
 })
